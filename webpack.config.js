6d30163fd4ac8192d593f50120a018c919bac894
Antony Ng'ang'a <antony_mn@Antonys-MBP.mshome.net>
2017-07-31 03:50:28 +0300

    webpack and ReactJs project setup complete

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const path = require('path');
+const glob = require('glob');
+const webpack = require("webpack");
+const PurifyCSSPlugin = require('purifycss-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const bootstrapEntryPoints = require('./webpack.bootstrap.config');
 
 const isProd = process.env.NODE_ENV === "production";
 const cssDev = ['style-loader', 'css-loader', 'sass-loader'];
@@ -8,23 +12,51 @@ const cssProd = ExtractTextPlugin.extract({
     fallback: "style-loader",
     use: ["css-loader", 'sass-loader', 'css-loader?sourceMap!sass-loader?config=sassLoader'],
     publicPath: './dist'
-});
+})
 
 const cssConfig = isProd ? cssProd : cssDev;
+
+const bootstrapConfig = isProd ? bootstrapEntryPoints.prod : bootstrapEntryPoints.dev;
+
 
 module.exports = {
     entry: {
         app: './src/index.js',
+        bootstrap: bootstrapConfig
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].bundle.js'
     },
+    devtool: "source-map",
     module: {
         rules: [{
-            test: /(\.scss|\.css)$/,
-            use: cssConfig
-        }]
+                test: /(\.scss|\.css)$/,
+                use: cssConfig
+            }, {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: ["babel-loader", "source-map-loader"],
+                enforce: "pre"
+            }, {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loader: [
+                        // "file-loader?name=imgs/[name].[ext]",
+                        "file-loader?name=[name].[ext]&publicPath=&outputPath=imgs/",
+                        'image-webpack-loader'
+                    ] //custom output path for the images and hash name length
+            }, {
+                test: /\.(woff2?|svg)$/,
+                loader: 'url-loader?limit=10000&name=fonts/[name].[ext]'
+            }, {
+                test: /\.(ttf|eot|otf)$/,
+                loader: 'file-loader?name=fonts/[name].[ext]'
+            },
+            {
+                test: /bootstrap-sass\/assets\/javascripts\//,
+                loader: 'imports-loader?jQuery=jquery'
+            }
+        ]
     },
     devServer: {
         contentBase: path.join(__dirname, "dist"),
@@ -34,7 +66,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Grid Blog',
+            title: 'Grid Layout Blog',
             minify: {
                 collapseWhitespace: true
             },
@@ -43,10 +75,12 @@ module.exports = {
         }),
         new ExtractTextPlugin({
             filename: '[name].css',
-            allChunk: true,
+            allChunks: true,
             disable: !isProd
         }),
         new webpack.HotModuleReplacementPlugin({}),
+        new PurifyCSSPlugin({
+            paths: glob.sync(path.join(__dirname, 'src/index.ejs')),
+        })
     ]
-
 }
